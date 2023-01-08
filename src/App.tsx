e244d6021acca6a1a291a90e81e728aea982d20c
AelyPro <had.delengaigne@gmail.com>
2023-01-08 19:57:21 +0100

    load faction from json

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
-import React, { FC, useState, ChangeEvent, useEffect } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
 import FactionComponent from './components/FactionComponent';
 import { IFaction } from './types/IFaction';
+import factionList from './json/faction.json';
 
 const App: FC = () => {
     const CURRENT_PLAY_COUNT_ = 3;
     const CURRENT_TOTAL_PRESENCE_NEEDED = 17;
 
-    const [factionList, setFactionList] = useState<IFaction[]>([]);
     const [selectedList, setSelectedList] = useState<IFaction[]>([]);
-
-    const marquise = { id: 0, factionName: 'Marquise', presencePoint: 10 };
-    const duchy = { id: 1, factionName: 'Duchy', presencePoint: 8 };
-    const eyrie = { id: 2, factionName: 'Eyrie', presencePoint: 7 };
-    const vaga = { id: 3, factionName: 'Vaga', presencePoint: 5 };
-    const riverfolk = { id: 4, factionName: 'Riverfolk', presencePoint: 5 };
-    const alliance = { id: 5, factionName: 'Alliance', presencePoint: 3 };
-    const corvid = { id: 6, factionName: 'Corvid', presencePoint: 3 };
-    const vaga2 = { id: 7, factionName: 'Vaga2', presencePoint: 2 };
-    const cult = { id: 8, factionName: 'Cult', presencePoint: 2 };
-
-    useEffect(() => {
-        const createFactionList = () => {
-            console.log('Création de la liste de faction');
-            const factionList = [marquise, duchy, eyrie, vaga, riverfolk, alliance, corvid, vaga2, cult];
-            setFactionList(factionList);
-        };
-
-        createFactionList();
-    }, []);
 
     const handleFactionClick = (factionToCheck: IFaction): void => {
         const isSelected = selectedList.includes(factionToCheck);
         // Si c'est sélectionné on déselectionne
         if (isSelected) {
-            setSelectedList(selectedList.filter((faction) => faction.id != factionToCheck.id));
+            setSelectedList(selectedList.filter((faction) => faction.id !== factionToCheck.id));
         } else {
             setSelectedList([...selectedList, factionToCheck]);
         }
